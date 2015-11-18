36d91bb848d2e4a576442a4f688fef6f7447ffff
rosspa <rosspa@rose-hulman.edu>
2015-11-17 23:38:08 -0700

    created seeding file, with default map resolution of 50 by 50, and unranked through challenger, with 5 positions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+MAX_X = 50
+MAX_Y = 50
+RANKS = %w(unranked bronze silver gold platinum diamond master challenger)
+POSITIONS = %w(top jungle mid adc support)
+count = 0
+
+puts "starting seeding"
+RANKS.each do |rank|
+  POSITIONS.each do |position|
+    (0...MAX_X).each do |x|
+      (0...MAX_Y).each do |y|
+        Bucket.create(x: x, y: y, rank: rank, position: position, wins: 0, losses: 0)
+        count += 1
+      end
+    end
+    puts "done with #{rank} #{position}s"
+  end
+end
+puts "#{count} buckets created"
+puts "done seeding, have a great day! :)"
+
+
